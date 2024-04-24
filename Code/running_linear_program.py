71e26c8e8acb759d072c316aec001d22f6cb6cff
clancyandrews3 <clancyandrews@Clancy-Desktop>
2024-04-24 01:45:15 -0700

    Almost complete with constraints section. Started added code for output function

--- Code/running_linear_program.py
+++ Code/running_linear_program.py
@@ -2,7 +2,7 @@
 # Optimal Season Running Schedule
 # 
 #
-# Last Modified: April 18, 2024
+# Last Modified: April 21, 2024
 # Author: Clancy Andrews
 # Github: https://github.com/clancyandrews3/Optimal_Season_Running_Schedule
 ############################
@@ -15,19 +15,64 @@ import numpy as np
 def get_starting_mileage():
     """Gets and returns starting mileage from user"""
     
-    try:
-        start_mileage = int(input("\nEnter the starting mileage for training: "))
-    except:
-        ValueError("Value needs to be a number.")
+    while True:
+        try:
+            start_mileage = int(input("\nEnter the starting mileage for training: "))
+            break
+        except ValueError:
+            print("Value needs to be a number.")
     
     return start_mileage
 
 
 def output_results(results):
     """Output the results in a user friendly way"""
-    
-    for x_i in results:
-        pass
+
+    #Index for results vector
+    x_index = (0,335)
+    y_index = (336,671)
+    delta_index = (672,687)
+
+    #Variables needed for output
+    week = 1
+    days = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
+    workout_type = []
+    current_day = []
+    distance = []
+
+    #Iterate over results vector and get schedule information; print for user
+    for i in range(0,336,3):
+
+        #Print week results and update week to next week
+        if i % 7 == 0:
+            print("")
+            week += 1
+            print("\n\n")
+
+            #Reset lists for next week
+            workout_type = []
+            current_day = []
+            distance = []
+
+        #Set current day from dictionary
+        current_day.append(days[(i%7)+1])
+
+        #Check if run aerobic on day i
+        if results[i+y_index[0]] == 1:
+            workout_type.append("Aerobic Workout")
+            distance.append(results[i])
+
+        #Check if run anerobic on day i
+        elif results[(i+1)+y_index[0]] == 1:
+            workout_type.append("Anerobic Workout")
+            distance.append(results[(i+1)])
+
+        #Check if run threshold on day i
+        elif results[(i+2)+y_index[0]] == 1:
+            workout_type.append("Threshold Workout")
+            distance.append(results[(i+2)])
+
+        
 
 
 
@@ -57,9 +102,9 @@ if __name__=="__main__":
     #Specify integer variables
     x_i = [0 for i in range(336)]
     y_i = [1 for i in range(336)]
-    M_t = [0 for i in range(16)]
+    M_t = [0 for i in range(16)] # REMOVE
     d = [0 for i in range(16)]
-    isint = x_i + y_i + M_t+d
+    isint = x_i + y_i + M_t + d
 
     #Calculate results
 
