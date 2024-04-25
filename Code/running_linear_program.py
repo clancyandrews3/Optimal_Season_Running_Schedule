a6afa1110ed61f98d0d94f498605a712b0ddddb4
clancyandrews3 <clancyandrews@Clancy-Laptop.localdomain>
2024-04-25 14:28:53 -0700

    Started doing constraints in .py file

--- Code/running_linear_program.py
+++ Code/running_linear_program.py
@@ -78,23 +78,114 @@ def output_results(results):
 
 if __name__=="__main__":
 
-    dim = 704 #Depends on how many objective variables are in the program
+    dim = 736 #Depends on how many objective variables are in the program
 
     #Get starting milage
     mileage = get_starting_mileage()
     print(f"\nStarting Mileage (km): {mileage}\n\n")
     
 
+
+
+    #Determine goal weekly mileage for each week in the season
+    M = [mileage]
+    for t in range(13):
+        next_week = round(1.1*M[t],1) #10% increase from previous week
+        M.append(next_week)
+
+    for t in range(2):
+        next_week = round(0.8*M[-1],1) # Last 2 week are 20% fewer kilometers
+        M.append(next_week)
+
+
+
+
+    #Set up the weekly ratios of mileage for the season for each workout type
+    aerobic_mileage_ratio = []
+    anerobic_threshold_mileage_ratio = []
+    for t in range(16):
+        if t in range(4):
+            aerobic_mileage_ratio.append(0.8)
+            anerobic_threshold_mileage_ratio.append(0.2*0.5)
+        elif t in range(4,8):
+            aerobic_mileage_ratio.append(0.75)
+            anerobic_threshold_mileage_ratio.append(0.25*0.5)
+        elif t in range(8,12):
+            aerobic_mileage_ratio.append(0.7)
+            anerobic_threshold_mileage_ratio.append(0.3*0.5)
+        elif t in range(12,14):
+            aerobic_mileage_ratio.append(0.65)
+            anerobic_threshold_mileage_ratio.append(0.35*0.5)
+        else:
+            aerobic_mileage_ratio.append(0.6)
+            anerobic_threshold_mileage_ratio.append(0.4*0.5)
+
+
+
+
+    #Inequality and equality constraints stored here
+    A = np.array([[]])
+    Ae = None
+
+
+
+
+    ############### Start Aerobic Constraints #################
+
+    #Total weekly aerobic mileage 0.8-0.6 of total weekly mileage
+    for t in range(16):
+        x = [-1,0,0]*7
+        d = np.concatenate((np.zeros(t),np.array([1]),np.zeros(15-t)))
+        
+        
+
+
+
+
+
+    ############### End Aerobic Constraints ####################
+
+
+    ############### Start Anerobic Constraints #################
+
+
+
+
+
+
+
+    ############### End Anerobic Constraints ###################
+
+
+    ############### Start Threshold Constraints ################
+
+
+
+
+
+
+
+    ############### End Threshold Constraints ##################
+
+
+    ############### Start General Constraints ##################
+
+
+
+
+
+
+
+    ############### End General Constraints ####################
+
+
+
     #Objective function
     c = np.concatenate((np.zeros((dim-16)),np.ones((16))))
 
-    #Inequality and equality constraints
-    A = np.array([[]])
-    Ae = np.array([[]])
-
     #Constraint values
     b = np.array([])
-    be = np.array([])
+    be = None
 
     #Determine bounds
     bounds = ()
@@ -102,9 +193,11 @@ if __name__=="__main__":
     #Specify integer variables
     x_i = [0 for i in range(336)]
     y_i = [1 for i in range(336)]
-    M_t = [0 for i in range(16)] # REMOVE
-    d = [0 for i in range(16)]
-    isint = x_i + y_i + M_t + d
+    alpha = [0 for i in range(16)]
+    beta = [0 for i in range(16)]
+    gamma = [0 for i in range(16)]
+    delta = [0 for i in range(16)]
+    isint = x_i + y_i + alpha + beta + gamma + delta
 
     #Calculate results
 
