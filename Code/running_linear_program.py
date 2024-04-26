7afe60181f72bd120aeaa5b3ee7fb8bac7c7c28a
clancyandrews3 <clancyandrews@Clancy-Desktop>
2024-04-26 01:46:55 -0700

    Almost done with constraints. Issue is that I need to correlate xi and yi values

--- Code/running_linear_program.py
+++ Code/running_linear_program.py
@@ -2,7 +2,7 @@
 # Optimal Season Running Schedule
 # 
 #
-# Last Modified: April 21, 2024
+# Last Modified: April 25, 2024
 # Author: Clancy Andrews
 # Github: https://github.com/clancyandrews3/Optimal_Season_Running_Schedule
 ############################
@@ -73,7 +73,9 @@ def output_results(results):
             distance.append(results[(i+2)])
 
         
-
+def print_matrix(A):
+    for i in A:
+        print(i)
 
 
 if __name__=="__main__":
@@ -123,9 +125,30 @@ if __name__=="__main__":
 
 
 
-    #Inequality and equality constraints stored here
-    A = np.array([[]])
-    Ae = None
+    #Starting with linearizing the objective function
+    x = [-1, -1, -1] * 7
+    d = np.concatenate((np.zeros(0), np.array([1]), np.zeros(15)))
+
+    first_row = np.concatenate((np.zeros(0), x, np.zeros(dim - 37), d))
+    A = np.array([first_row])
+    b = [-M[0]]
+    
+    for t in range(1,16):
+        x = [-1, -1, -1] * 7
+        d = np.concatenate((np.zeros(t), np.array([1]), np.zeros(15 - t)))
+
+        new_row = np.concatenate((np.zeros(t * 21), x, np.zeros((dim - 37) - (21 * t)), d))
+        A = np.vstack([A, new_row])
+        b.append(-M[t])
+        
+
+    for t in range(16):
+        x = [-1, -1, -1] * 7
+        d = np.concatenate((np.zeros(t), np.array([-1]), np.zeros(15 - t)))
+
+        new_row = np.concatenate((np.zeros(t * 21), x, np.zeros((dim - 37) - (21 * t)), d))
+        A = np.vstack([A, new_row])
+        b.append(-M[t])
 
 
 
@@ -134,61 +157,171 @@ if __name__=="__main__":
 
     #Total weekly aerobic mileage 0.8-0.6 of total weekly mileage
     for t in range(16):
+
         x = [-1,0,0]*7
         d = np.concatenate((np.zeros(t),np.array([1]),np.zeros(15-t)))
+
+        new_row = np.concatenate((np.zeros(t*21), x, np.zeros(315-(21*t)),np.zeros(336),d,np.zeros(48)))
         
+        A = np.vstack([A,new_row])
+        b.append(-round((aerobic_mileage_ratio[t]*M[t]),1))
         
+    for t in range(16):
+        x = [-1,0,0]*7
+        d = np.concatenate((np.zeros(t),np.array([-1]),np.zeros(15-t)))
+
+        new_row = np.concatenate((np.zeros(t*21), x, np.zeros(315-(21*t)),np.zeros(336),d,np.zeros(48)))
+
+        A = np.vstack([A,new_row])
+        b.append(-round((aerobic_mileage_ratio[t]*M[t]),1))
 
 
 
+    #Longest aerobic run no longer than 30% total goal mileage
+    for t in range(16*7):
+        next_row = np.concatenate((np.zeros(3*t),np.array([1]),np.zeros(336-(3*t)-1),np.zeros(dim-336)))
 
+        A = np.vstack([A,next_row])
+
+    for t in range(16):
+        for _ in range(7):
+            b.append(0.3*M[t])
+
+    
+
+    # 
 
     ############### End Aerobic Constraints ####################
 
 
+
+
     ############### Start Anerobic Constraints #################
 
+    #Total weekly anerobic mileage 0.1-0.2 of total weekly mileage
+    for t in range(16):
+
+        x = [0,-1,0]*7
+        d = np.concatenate((np.zeros(t),np.array([1]),np.zeros(15-t)))
+
+        new_row = np.concatenate((np.zeros(t*21), x, np.zeros(315-(21*t)),np.zeros(336+16),d,np.zeros(32)))
+        
+        A = np.vstack([A,new_row])
+        b.append(-round((anerobic_threshold_mileage_ratio[t]*M[t]),1))
+        
+    for t in range(16):
+        x = [0,-1,0]*7
+        d = np.concatenate((np.zeros(t),np.array([-1]),np.zeros(15-t)))
+
+        new_row = np.concatenate((np.zeros(t*21), x, np.zeros(315-(21*t)),np.zeros(336+16),d,np.zeros(32)))
+
+        A = np.vstack([A,new_row])
+        b.append(-round((anerobic_threshold_mileage_ratio[t]*M[t]),1))
 
 
 
+    #Only 3 anerobic workouts a week
+    for t in range(16):
+        y = [0,1,0]*7
+
+        next_row = np.concatenate((np.zeros(336),np.zeros(21*t),y,np.zeros(336-(21*t)-21),np.zeros(64)))
+        A = np.vstack([A,next_row])
+        b.append(3)
 
 
+
+    #Cannot do two consecutive anerobic workouts
+    for t in range(16*7):
+        next_row = np.concatenate((np.zeros(336),np.zeros(t),np.array([1,1]),np.zeros(336-t-2),np.zeros(64)))
+
+        A = np.vstack([A,next_row])
+        b.append(1)
 
     ############### End Anerobic Constraints ###################
 
 
+
+
     ############### Start Threshold Constraints ################
 
+    #Total weekly aerobic mileage 0.8-0.6 of total weekly mileage
+    for t in range(16):
+
+        x = [0,0,-1]*7
+        d = np.concatenate((np.zeros(t),np.array([1]),np.zeros(15-t)))
+
+        new_row = np.concatenate((np.zeros(t*21), x, np.zeros(315-(21*t)),np.zeros(336+32),d,np.zeros(16)))
+        
+        A = np.vstack([A,new_row])
+        b.append(-round((anerobic_threshold_mileage_ratio[t]*M[t]),1))
+        
+    for t in range(16):
+        x = [0,0,-1]*7
+        d = np.concatenate((np.zeros(t),np.array([-1]),np.zeros(15-t)))
+
+        new_row = np.concatenate((np.zeros(t*21), x, np.zeros(315-(21*t)),np.zeros(336+32),d,np.zeros(16)))
+
+        A = np.vstack([A,new_row])
+        b.append(-round((anerobic_threshold_mileage_ratio[t]*M[t]),1))
 
 
 
+    #Longest threshold run no longer than 10% total goal mileage
+    for t in range(16*7):
+        next_row = np.concatenate((np.zeros(3*t+2),np.array([1]),np.zeros(336-(3*t)-1-2),np.zeros(dim-336)))
+        print(next_row)
 
+        A = np.vstack([A,next_row])
 
+    for t in range(16):
+        for _ in range(7):
+            b.append(0.1*M[t])
 
     ############### End Threshold Constraints ##################
 
 
+
+
     ############### Start General Constraints ##################
 
+    #Between 3 and 6 runs per week
+    for t in range(16):
+        y = [1]*21
+        next_row = np.concatenate((np.zeros(336+(21*t)),y,np.zeros(dim-336-21-(21*t))))
+        A = np.vstack([A,next_row])
+        b.append(6)
+
+    for t in range(16):
+        y = [-1]*21
+        next_row = np.concatenate((np.zeros(336+(21*t)),y,np.zeros(dim-336-21-(21*t))))
+        A = np.vstack([A,next_row])
+        b.append(-3)
 
 
 
-
-
+    #Only one run a day
+    for t in range(16*7):
+        y = [1,1,1]
+        next_row = np.concatenate((np.zeros(336+(3*t)),y,np.zeros(397-(3*t))))
+        A = np.vstack([A,next_row])
+        b.append(1)
 
     ############### End General Constraints ####################
 
+    
 
 
     #Objective function
     c = np.concatenate((np.zeros((dim-16)),np.ones((16))))
 
-    #Constraint values
-    b = np.array([])
-    be = None
+
 
     #Determine bounds
-    bounds = ()
+    bounds = [(0,np.inf) for _ in range(336)]
+    bounds.extend((0,1) for _ in range(336))
+    bounds.extend((0,np.inf) for _ in range(64))
+
+
 
     #Specify integer variables
     x_i = [0 for i in range(336)]
@@ -199,8 +332,13 @@ if __name__=="__main__":
     delta = [0 for i in range(16)]
     isint = x_i + y_i + alpha + beta + gamma + delta
 
+
+
     #Calculate results
+    results=opt.linprog(c,A,b,None,None,bounds,integrality = isint)
 
 
+    print(A.shape)
     #Output results
-    
+    print(f"Optimal Objective Value (z): {results['fun']}")
+    print(f"Optimal Decision Variable Vector (x): {results['x']}\n\n")
